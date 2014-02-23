385c43f7ae15d1a99cd406d59b37c57265637e3f
Karsten Rowe <krowe@nutshell.com>
2014-02-23 15:58:55 +0000

    Initial commit of project.

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+    </div> <!-- /container -->
+
+    <footer class="container">
+      <p>© Company Name 2014</p>
+    </footer>
+
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,4 @@
+<!-- <h2>Sidebar</h2> -->
+<div class="widget">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div>
