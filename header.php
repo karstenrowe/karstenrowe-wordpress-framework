8b9659f005776354e9942421482588cbd68577a4
Karsten Rowe <krowe@nutshell.com>
2014-02-25 18:13:00 +0000

    Be a little more intelligent with the favicon.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="karstenrowe bootstrap wordpress framework">
     <meta name="author" content="karstenrowe">
-    <link rel="shortcut icon" href="ico/favicon.png">
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/ico/favicon.png" />
 
 		<!-- Typekit fonts import -->
 		<script type="text/javascript" src="//use.typekit.net/qpb8voj.js"></script>
